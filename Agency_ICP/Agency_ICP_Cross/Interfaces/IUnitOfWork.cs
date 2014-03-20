97b36f836121dc807a51a23fcc21144257bef4fe
宋志义 <antsong@outlook.com>
2014-03-20 17:27:39 +0800

    3-21

---
+++ Agency_ICP/Agency_ICP_Cross/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Agency_ICP_Cross.Interfaces
+{
+    public interface IUnitOfWork
+    {
+    }
+}
